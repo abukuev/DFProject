fa24a0552e51dba8faed339749adc0453d24326b
abukuev <abukuev@list.ru>
2024-05-31 20:28:18 +0300

    DB added

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -21,5 +21,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.Home),
+    path('', views.home),
+    path('items', views.tems),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 
-def Home(request):
+def home(request):
 
     text = "</h1>HELLO</h1>"
     context={'name':'Алексей', 'surname':'Букуев'}
-    
+
     return render(request,'index.html',context)
+
+
+
+def items(request):
+    pass
